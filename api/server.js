f99b8211e06fdd4cc7b956617426594b8cae40e3
Dhananjay Puranik <dhananjaypuranik8@gmail.com>
2024-10-04 13:22:47 +0530

    completed shipping api

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ import bodyParser from "express";
 import userRouter from "./Routes/user.js";
 import productRouter from "./Routes/product.js";
 import cartRouter from "./Routes/cart.js";
+import addressRouter from "./Routes/address.js";
 const app = express();
 app.use(bodyParser.json());
 //home route
@@ -20,6 +21,10 @@ app.use("/api/product", productRouter);
 //cart router
 
 app.use("/api/cart", cartRouter);
+
+//address router
+
+app.use("/api/address", addressRouter);
 
 const db =
   "mongodb+srv://dhananjaypuranik8:dhananjay@cluster0.n67j8.mongodb.net/";
