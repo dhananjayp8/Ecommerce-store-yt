f9f38cb563d398209b0699a7ce9ea572e1e27499
Dhananjay Puranik <dhananjaypuranik8@gmail.com>
2024-10-05 15:59:37 +0530

    product component setup

--- client/src/components/product/ShowProduct.jsx
+++ client/src/components/product/ShowProduct.jsx
@@ -3,13 +3,49 @@ import AppContext from "../../context/AppContext";
 const ShowProduct = () => {
   const { products } = useContext(AppContext);
   return (
-    <div>
-      {products.map((product) => (
-        <div key={product._id}>
-          <h1>{product.title}</h1>
+    <>
+      <div className="container flex justify-content-center align-items-center">
+        <div className="row  container d-flex justify-content-center align-items-center my-5">
+          {products.map((product) => (
+            <div
+              key={product._id}
+              className=" my-3 col-md-4 d-flex justify-content-center align-items-center"
+            >
+              <div
+                className="card bg-dark text-light text-center"
+                style={{ width: "18rem" }}
+              >
+                <div className="d-flex justify-content-center align-items-center p-3">
+                  <img
+                    src={product.imgSrc}
+                    className="card-img-top"
+                    alt="..."
+                    style={{
+                      width: "200px",
+                      height: "200px",
+                      borderRadius: "10px",
+                      border: "2px solid yellow",
+                    }}
+                  />
+                </div>
+
+                <div className="card-body">
+                  <h5 className="card-title">{product.title}</h5>
+                  <div className="my-3 d-flex">
+                    <button href="#" className="btn btn-primary mx-2">
+                      Rs. {product.price}
+                    </button>
+                    <button href="#" className="btn btn-warning mx-2">
+                      Add to Cart
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      </div>
+    </>
   );
 };
 
